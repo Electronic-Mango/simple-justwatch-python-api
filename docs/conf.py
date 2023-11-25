469c7068c1014265ff035de9e74f68e03fcc922d
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-11-25 16:21:21 +0100

    Reformat Sphinx conf.py file

--- docs/conf.py
+++ docs/conf.py
@@ -8,28 +8,24 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/'))
 
-project = 'Simple JustWatch Python API'
-copyright = '2023, Electronic-Mango'
-author = 'Electronic-Mango'
+sys.path.insert(0, os.path.abspath("../src/"))
+
+project = "Simple JustWatch Python API"
+copyright = "2023, Electronic-Mango"
+author = "Electronic-Mango"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]

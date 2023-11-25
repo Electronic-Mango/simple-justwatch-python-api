ad842c577a786bf488d3f14cc4a412cf465f982a
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-11-25 12:38:48 +0100

    Add unit tests to "justwatch" module

---
+++ test/simplejustwatchpythonapi/test_justwatch.py
@@ -0,0 +1,24 @@
+from unittest.mock import MagicMock, patch
+
+from simplejustwatchpythonapi.justwatch import search
+
+JUSTWATCH_GRAPHQL_URL = "https://apis.justwatch.com/graphql"
+SEARCH_INPUT = ("TITLE", "COUNTRY", "LANGUAGE", 5, True)
+DUMMY_REQUEST = {"dummy": "request"}
+DUMMY_RESPONSE = {"dummy": "response"}
+DUMMY_ENTRIES = [MagicMock(), MagicMock(), None]
+
+
+@patch("simplejustwatchpythonapi.justwatch.post")
+@patch("simplejustwatchpythonapi.justwatch.parse_search_response", return_value=DUMMY_ENTRIES)
+@patch("simplejustwatchpythonapi.justwatch.prepare_search_request", return_value=DUMMY_REQUEST)
+def test_search(requests_mock, parser_mock, httpx_mock) -> None:
+    httpx_mock().json.return_value = DUMMY_RESPONSE
+
+    results = search(*SEARCH_INPUT)
+
+    requests_mock.assert_called_with(*SEARCH_INPUT)
+    httpx_mock.assert_called_with(JUSTWATCH_GRAPHQL_URL, json=DUMMY_REQUEST)
+    httpx_mock().raise_for_status.assert_called()
+    parser_mock.assert_called_with(DUMMY_RESPONSE)
+    assert results == DUMMY_ENTRIES

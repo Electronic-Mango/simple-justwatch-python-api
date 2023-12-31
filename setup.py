d1bc2bbfa046e780cc1166b35c0d0e7444dc8049
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2023-12-31 18:32:12 +0100

    Update version to 0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="simple-justwatch-python-api",
     packages=["simplejustwatchapi"],
     package_dir={"simplejustwatchapi": "src/simplejustwatchapi"},
-    version="0.8",
+    version="0.9",
     license="GPLv3",
     description="A simple JustWatch Python API",
     long_description=long_description,

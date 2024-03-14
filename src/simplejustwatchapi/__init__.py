c3491a8add0529ccde3cd9c644b83ecd06f51994
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-03-14 15:22:50 +0100

    Add missing imports to __init__.py

--- src/simplejustwatchapi/__init__.py
+++ src/simplejustwatchapi/__init__.py
@@ -1,2 +1,2 @@
-from simplejustwatchapi.justwatch import details, search
-from simplejustwatchapi.query import MediaEntry, Offer
+from simplejustwatchapi.justwatch import details, offers_for_countries, search
+from simplejustwatchapi.query import MediaEntry, Offer, OfferPackage

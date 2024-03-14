43ae133deeb55a6e2fa7eeabfbfe74c696ac5c04
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-03-14 15:32:35 +0100

    Add missing unit tests from "justwatch" module

--- test/simplejustwatchapi/test_justwatch.py
+++ test/simplejustwatchapi/test_justwatch.py
@@ -2,11 +2,15 @@ from unittest.mock import MagicMock, patch
 
 from pytest import fixture
 
-from simplejustwatchapi.justwatch import details, search
+from simplejustwatchapi.justwatch import details, offers_for_countries, search
 
 JUSTWATCH_GRAPHQL_URL = "https://apis.justwatch.com/graphql"
+
 SEARCH_INPUT = ("TITLE", "COUNTRY", "LANGUAGE", 5, True)
 DETAILS_INPUT = ("NODE ID", "COUNTRY", "LANGUAGE", False)
+OFFERS_COUNTRIES_INPUT = {"COUNTRY1", "COUNTRY2", "COUNTRY3"}
+OFFERS_INPUT = ("NODE ID", OFFERS_COUNTRIES_INPUT, "LANGUAGE", True)
+
 DUMMY_REQUEST = {"dummy": "request"}
 DUMMY_RESPONSE = {"dummy": "response"}
 DUMMY_ENTRIES = [MagicMock(), MagicMock(), None]
@@ -37,3 +41,21 @@ def test_details(requests_mock, parser_mock, httpx_post_mock):
     requests_mock.assert_called_with(*DETAILS_INPUT)
     parser_mock.assert_called_with(DUMMY_RESPONSE)
     assert results == DUMMY_ENTRIES
+
+
+@patch(
+    "simplejustwatchapi.justwatch.parse_offers_for_countries_response", return_value=DUMMY_ENTRIES
+)
+@patch(
+    "simplejustwatchapi.justwatch.prepare_offers_for_countries_request", return_value=DUMMY_REQUEST
+)
+def test_offers_for_countries(requests_mock, parser_mock, httpx_post_mock):
+    results = offers_for_countries(*OFFERS_INPUT)
+    requests_mock.assert_called_with(*OFFERS_INPUT)
+    parser_mock.assert_called_with(DUMMY_RESPONSE, OFFERS_COUNTRIES_INPUT)
+    assert results == DUMMY_ENTRIES
+
+
+def test_offers_for_countries_returns_empty_dict_for_empty_countries_set():
+    results = offers_for_countries("", set(), "", False)
+    assert not results

580feadc530be7cbcca8cba3d0348381a8d7225e
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-03-28 14:35:07 +0100

    Migrate documentation workflow to uv

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,17 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../src/"))
-
 project = "Simple JustWatch Python API"
 copyright = "2023, Electronic-Mango"
 author = "Electronic-Mango"
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["docs/build"]
 
 
 # -- Options for HTML output -------------------------------------------------

9ba23d57959306f1c74a99e7021baaf0e3127ee9
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-04-02 23:23:42 +0200

    Update messages and fields in exceptions

--- src/simplejustwatchapi/exceptions.py
+++ src/simplejustwatchapi/exceptions.py
@@ -6,7 +6,13 @@ class JustWatchError(Exception):
 
 
 class JustWatchCountryCodeError(JustWatchError):
-    """Raise when user provided invalid country code."""
+    """
+    Raise when user provided invalid country code.
+
+    Attributes:
+        code (str): Invalid country code which caused this exception.
+
+    """
 
     def __init__(self, code: str) -> None:
         """Init JustWatchCountryCodeError with invalid country code."""
@@ -15,7 +21,20 @@ class JustWatchCountryCodeError(JustWatchError):
 
 
 class JustWatchHttpError(JustWatchError):
-    """Raise when JustWatch API returned a non-2xx status code."""
+    """
+    Raise when JustWatch API returned a non-2xx status code.
+
+    Attributes:
+        code (int): HTTP status code received from API.
+        message (str): Message received from API, alongside the non-2xx status code.
+
+    """
+
+    def __init__(self, code: int, message: str) -> None:
+        """Init JustWatchHttpError with status code and message from response."""
+        super().__init__(f"{code}: {message}")
+        self.code = code
+        self.message = message
 
 
 class JustWatchApiError(JustWatchError):
@@ -23,15 +42,16 @@ class JustWatchApiError(JustWatchError):
     Raise when JustWatch API returned errors in JSON response.
 
     If this error is raised, then API responded with status code 2xx, but there are listed errors
-    in the internal JSON response.
+    in the internal JSON response. It can happen for too high complexity of request, or invalid
+    node ID in functions like :func:`simplejustwatchapi.details`.
+
+    Attributes:
+        errors (list[dict]): List of errors from JSON response. Each dict contain at least two
+            keys - "message" and "code".
+
     """
 
-
-class JustWatchTooHighComplexityError(JustWatchApiError):
-    """
-    Raise when JustWatch API responded with too high complexity error.
-
-    This specifically means that API returned status code 2xx, but the query result would be too
-    complex for JustWatch API to handle. Usually caused by :func:`search` or :func:`popular` with
-    too high value of ``count``.
-    """
+    def __init__(self, errors: list[dict]) -> None:
+        """Init JustWatchApiError with internal errors from JSON response."""
+        super().__init__(f"Errors from JSON response: {errors}")
+        self.errors = errors

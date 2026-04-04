093d5ef88d875a67d4cf6a2159f6ecbdf8c04993
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-04-05 00:39:51 +0200

    Tweak docstrings
    Putting off update to docstrings in "justwatch.py"...

--- src/simplejustwatchapi/exceptions.py
+++ src/simplejustwatchapi/exceptions.py
@@ -19,6 +19,28 @@ class JustWatchError(Exception):
     """Common parent for all exceptions raised by this library."""
 
 
+class JustWatchApiError(JustWatchError):
+    """
+    Raised when JustWatch API returned errors in JSON response.
+
+    If this error is raised, then API responded with status code `2xx`, but there are
+    listed errors in the internal JSON response. It can happen for too high complexity
+    of request, invalid node ID in functions like [`details`]
+    [simplejustwatchapi.justwatch.details], or invalid country or language codes.
+
+    Attributes:
+        errors (list[dict]): List of all errors in the JSON response from the API.
+            The `dict` elements are themselves basic JSONs, each with at least two
+            keys - `message` and `code`.
+
+    """
+
+    def __init__(self, errors: list[dict]) -> None:
+        """Init JustWatchApiError with internal errors from JSON response."""
+        super().__init__(f"Errors in JSON response: {errors}")
+        self.errors = errors
+
+
 class JustWatchHttpError(JustWatchError):
     """
     Raised when JustWatch API returned a non-`2xx` status code.
@@ -36,25 +58,3 @@ class JustWatchHttpError(JustWatchError):
         super().__init__(f"HTTP code {code}: {message}")
         self.code = code
         self.message = message
-
-
-class JustWatchApiError(JustWatchError):
-    """
-    Raised when JustWatch API returned errors in JSON response.
-
-    If this error is raised, then API responded with status code `2xx`, but there are
-    listed errors in the internal JSON response. It can happen for too high complexity
-    of request, invalid node ID in functions like [`details`]
-    [simplejustwatchapi.justwatch.details], or invalid country codes, or languages.
-
-    Attributes:
-        errors (list[dict]): List of all errors in the JSON response from the API.
-            The `dict` elements are themselves basic JSONs, each with at least two
-            keys - `message` and `code`.
-
-    """
-
-    def __init__(self, errors: list[dict]) -> None:
-        """Init JustWatchApiError with internal errors from JSON response."""
-        super().__init__(f"Errors in JSON response: {errors}")
-        self.errors = errors
